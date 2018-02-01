2e99b179f1c3b6268d61b65821158d79fbe439dd
ben awad <benawad97@gmail.com>
2018-02-01 12:04:30 -0600

    login response

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -18,17 +18,29 @@ export const auth = {
   async login(parent, { email, password }, ctx: Context, info) {
     const user = await ctx.db.query.user({ where: { email } })
     if (!user) {
-      throw new Error(`No such user found for email: ${email}`)
+      return {
+        error: {
+          field: 'email',
+          msg: 'No user found',
+        }
+      }
     }
 
     const valid = await bcrypt.compare(password, user.password)
     if (!valid) {
-      throw new Error('Invalid password')
+      return {
+        error: {
+          field: 'password',
+          msg: 'Invalid password',
+        }
+      }
     }
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-      user,
+      payload: {
+        token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+        user,
+      }
     }
   },
 }
