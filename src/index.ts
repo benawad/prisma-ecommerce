01d7db8398e076601af91e61d8969b7950007295
ben awad <benawad97@gmail.com>
2018-02-08 10:22:22 -0600

    server static images

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Prisma } from './generated/prisma'
-import resolvers from './resolvers'
+import { GraphQLServer } from "graphql-yoga";
+const express = require("express");
+
+import { Prisma } from "./generated/prisma";
+import resolvers from "./resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in .env)
       secret: process.env.PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
-      debug: true, // log all GraphQL queries & mutations
-    }),
-  }),
-})
+      debug: true // log all GraphQL queries & mutations
+    })
+  })
+});
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.express.use("/images", express.static("images"));
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
