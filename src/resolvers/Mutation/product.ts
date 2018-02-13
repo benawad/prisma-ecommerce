9130dd2c94bc2d0cde05902da8ae38cd01753b64
ben awad <benawad97@gmail.com>
2018-02-13 13:08:35 -0600

    add mutation to edit and delete products

--- src/resolvers/Mutation/product.ts
+++ src/resolvers/Mutation/product.ts
@@ -1,5 +1,6 @@
 import { createWriteStream } from "fs";
 import * as shortid from "shortid";
+import { forwardTo } from "prisma-binding";
 
 import { getUserId, Context } from "../../utils";
 
@@ -21,6 +22,39 @@ const processUpload = async upload => {
 };
 
 export const product = {
+  deleteProduct: forwardTo("db"),
+  async updateProduct(
+    parent,
+    { id, name, price, picture },
+    ctx: Context,
+    info
+  ) {
+    const userId = getUserId(ctx);
+    const product = await ctx.db.query.product({ where: { id } });
+    console.log(product);
+    if (userId !== product.seller.id) {
+      throw new Error("Not authorized");
+    }
+
+    let pictureUrl = null;
+    if (picture) {
+      pictureUrl = await processUpload(picture);
+    }
+
+    return ctx.db.mutation.updateProduct(
+      {
+        data: {
+          name,
+          price,
+          pictureUrl
+        },
+        where: {
+          id
+        }
+      },
+      info
+    );
+  },
   async createProduct(parent, { name, price, picture }, ctx: Context, info) {
     const userId = getUserId(ctx);
     const pictureUrl = await processUpload(picture);
