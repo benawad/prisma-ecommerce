262208603cadad420619b08ca9c9cdfb9dc8e3e2
ben awad <benawad97@gmail.com>
2018-02-19 15:44:07 -0600

    add subs

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,15 +1,22 @@
-import { Query } from './Query'
-import { auth } from './Mutation/auth'
-import { post } from './Mutation/post'
-import { product } from './Mutation/product'
-import { AuthPayload } from './AuthPayload'
+import { Query } from "./Query";
+import { auth } from "./Mutation/auth";
+import { post } from "./Mutation/post";
+import { product } from "./Mutation/product";
+import { AuthPayload } from "./AuthPayload";
+import { Context } from "../utils";
 
 export default {
+  Subscription: {
+    product: {
+      subscribe: async (parent, args, ctx: Context, info) =>
+        ctx.db.subscription.product({}, info)
+    }
+  },
   Query,
   Mutation: {
     ...auth,
     ...post,
-    ...product,
+    ...product
   },
-  AuthPayload,
-}
+  AuthPayload
+};
